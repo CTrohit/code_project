c8735404b2129ead1e40fa3df1575267a0b0841a
CTrohit <rohit.patil@cubictree.com>
2021-12-28 19:12:13 +0530

    add new code1 file task-8

--- code1.rb
+++ code1.rb
@@ -4,17 +4,28 @@
 require 'net/http'
 require 'json'
 
-link = "https://www.ruby-lang.org/"
 
-links = URI.parse(link)
-response = Net::HTTP.get_response(links)
-puts response.body
-begin
-  file = File.open("/home/user1/Downloads/data.txt","w")
-  file.write(response.body)
-rescue IOError => e 
-  puts e
-ensure
-  file.close unless file.nil?
+def link(uri_str, limit = 10)
+ 
+  raise ArgumentError, 'too many HTTP redirects' if limit == 0
+  response = Net::HTTP.get_response(URI(uri_str))
+  case response
+  when Net::HTTPSuccess then
+    file = File.open("/home/user1/Downloads/data.txt","w")
+    file.write(response.body)
+    puts response.body
+    
+  when Net::HTTPRedirection then
+    location = response['location']
+    warn "redirected to #{location}"
+    link(location, limit - 1)
+  
+  else
+    response.value
+  end
 end
+
+print link('https://www.ruby-lang.org/')
+
+
 

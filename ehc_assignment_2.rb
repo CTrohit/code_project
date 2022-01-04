910b163990bdfd3618e442e5e389449c23136e28
CTrohit <rohit.patil@cubictree.com>
2022-01-04 13:10:29 +0530

    ehc_assignment_push_4

--- ehc_assignment_2.rb
+++ ehc_assignment_2.rb
@@ -2,35 +2,33 @@
 require "net/http"
 require "uri"
 class EhcAssignment 
+  def crawl(date)
+    url = "https://services.ecourts.gov.in/ecourtindiaHC/cases/highcourt_causelist_qry.php"
+    request = Net::HTTP.post_form(URI(url),{"action_code" => "pulishedCauselist", "causelist_dt" => date,"state_code" => "1","dist_code" => "1","court_code" => "2"})
+    case request
+    when Net::HTTPSuccess then
+      file=File.open("./result.html","w")
+      file.write(request.body)
+      file.close()
+      puts "File Save Path:-#{File.expand_path('../result.html',__FILE__)}"
+      response=request.body
+      puts "Parsing Result:-"
+      puts parse(response.gsub(/\s+/," ").strip)  
+    end
+  end
   def parse(response)
     response_array=response.split("^#")
     parse_array=[]
     for num in (0..response_array.length()-1)
-        response_array_element = response_array[num].split("~") 
-        parse_array[num]={'serial_number':num+1,
+      response_array_element = response_array[num].split("~") 
+      parse_array[num]={'serial_number':num+1,
                           'bench':response_array_element[1],
                           'cause_list':response_array_element[2],
                           'link':"https://services.ecourts.gov.in/ecourtindiaHC/cases/display_causelist.php?filename="+response_array_element[4]}
     end
     return parse_array
   end
-  def crawl(date)
-    raise ArgumentError, 'too many HTTP redirects' if (date.nil?)
-    url = "https://services.ecourts.gov.in/ecourtindiaHC/cases/highcourt_causelist_qry.php"
-    request = Net::HTTP.post_form(URI.parse(url),{"action_code" => "pulishedCauselist", "causelist_dt" => date,"state_code" => "1","dist_code" => "1","court_code" => "2"})
-    case request
-    when Net::HTTPSuccess then
-      file=File.open("./result.html","w")
-      file.write(request.body)
-      file.close()
-      response=request.body.gsub(/\s+/," ").strip
-      puts parse(response)  
-    else
-      request.value
-    end
-  end
 end
-
 puts "Enter a Date:DD-MM-YYYY"
 date=gets.chomp
 main=EhcAssignment.new
